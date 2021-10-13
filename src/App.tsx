4ea365f1d9853405cbfa68def6b911019c9cfc04
janthony-ud <janthony@udel.edu>
2021-10-13 00:15:01 -0400

    janthony- added names

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App(): JSX.Element {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 UD CIS Scheduler
-                Setting it all up
+                Andrew McCracken, Luis Garcia, Justin Anthony
                 <p>
                     Edit <code>src/App.tsx</code> and save to reload.
                 </p>
