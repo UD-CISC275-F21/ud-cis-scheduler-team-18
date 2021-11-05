4c40062290c6490c182249b7ac964dd15bd4d1c2
LuisGarciaOsorio <54846733+LuisGarciaOsorio@users.noreply.github.com>
2021-11-04 23:56:49 -0400

    added and implemented course interface will need to find a way to read through a json and create all the courses to add to interface

---
+++ src/CourseOverview.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Course from "./Interfaces/course";
+interface courseProps {
+  courses: Course[];
+}
+export const CourseOverview: React.FC<courseProps> = ({ courses }: courseProps) => {
+  return <div className="Course-Overview">
+    <table>
+      <tbody>
+        <tr className="Table-Header">
+          <td>
+            <h4>Department</h4>
+          </td>
+          <td>
+            <h4>Course</h4></td>
+          {/* <td>
+          <h4>
+            CoreReq
+          </h4>
+        </td>
+        <td>
+          <h4>
+            PreReq
+          </h4>
+        </td> */}
+        </tr>
+        {/* {courses.map(item=> {
+          return <CourseItem Department = {item.Department}
+          ID = {item.ID}>
+          </CourseItem>
+        })} */}
+      </tbody>
+    </table>
+  </div>;
+};
