93462a941b9d85ab943e35bcd8ebc05a6ce7ab58
mccrackudel <72521821+mccrackudel@users.noreply.github.com>
2021-12-08 17:33:25 -0500

    added a credit tracker per semester

--- src/components/SemesterViewer.tsx
+++ src/components/SemesterViewer.tsx
@@ -5,37 +5,19 @@ import { Semester } from "../interfaces/semester";
 
 
 export function SemesterViewer({sem}: {sem: Semester}): JSX.Element {
-	/*
 	let retval = "";
-	retval += sem.name;
-	retval += ":\n";
-	//sem.classA = "cisc275";
-	if(sem.classA != "none"){
-		retval += sem.classA;
-		retval += "\n";
-	}
-	if(sem.classB != "none"){
-		retval += sem.classB;
-		retval += "\n";
-	}
-	if(sem.classC != "none"){
-		retval += sem.classC;
-		retval += "\n";
-	}
-	if(sem.classD != "none"){
-		retval += sem.classD;
-		retval += "\n";
-	}
-	*/
-	
-	let retval2 = "";
-	retval2 += sem.semName + ": ";
+	retval += sem.semName + ": ";
 	for(const x in sem.courseLoad){
-		retval2 += sem.courseLoad[x].name + ", ";
+		retval += sem.courseLoad[x].name + ", ";
+	}
+	let credits = 0;
+	for(const x in sem.courseLoad){
+		credits += sem.courseLoad[x].credits;
 	}
 	
 	return <Row>
 		<h2>Semesters:</h2>
-		<div>{retval2}</div>
+		<h5>Total Credits: {credits}</h5>
+		<div>{retval}</div>
 	</Row>;
 }
