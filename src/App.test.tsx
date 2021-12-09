7407478231d78dbe5e310c426d444fe3a3de1065
mccrackudel <72521821+mccrackudel@users.noreply.github.com>
2021-12-08 20:25:13 -0500

    added some basic rendering tests...

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -8,3 +8,33 @@ test("renders UD CIS Scheduler text", () => {
     expect(linkElement).toBeInTheDocument();
 });
 
+test("renders Course List", () => {
+	render(<App />);
+    const linkElement = screen.getByText(/Course List:/i);
+    expect(linkElement).toBeInTheDocument();
+});
+
+test("renders Fulfilled Requirements and unfulfilled requirements", () => {
+	render(<App />);
+    const linkElement = screen.getAllByText(/Fulfilled Requirements:/i);
+    expect(linkElement[0]).toBeInTheDocument();
+	expect(linkElement[1]).toBeInTheDocument();
+});
+
+test("renders Control Panel", () => {
+	render(<App />);
+    const linkElement = screen.getByText(/Control Panel/i);
+    expect(linkElement).toBeInTheDocument();
+});
+
+test("Class Select", () => {
+	render(<App />);
+    const linkElement = screen.getByText(/Class Select/i);
+    expect(linkElement).toBeInTheDocument();
+});
+
+test("renders Semester Viewer", () => {
+	render(<App />);
+    const linkElement = screen.getByText(/Semesters:/i);
+    expect(linkElement).toBeInTheDocument();
+});
